e8cb37356b702491e7bbf054459d90bf97747201
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-10-29 11:37:02 +0100

    ocamlPackages.fiat-p256: 2.2.1 → 2.2.2

--- pkgs/development/ocaml-modules/fiat-p256/default.nix
+++ pkgs/development/ocaml-modules/fiat-p256/default.nix
@@ -1,17 +1,25 @@
 { lib, buildDunePackage, fetchurl, alcotest, asn1-combinators, benchmark
 , bigarray-compat, cstruct, eqaf, hex, ppx_deriving_yojson, rresult
-, stdlib-shims, yojson, dune-configurator }:
+, stdlib-shims, yojson, dune-configurator, pkg-config }:
 
 buildDunePackage rec {
   pname = "fiat-p256";
-  version = "0.2.1";
+  version = "0.2.2";
+
+  useDune2 = true;
 
   src = fetchurl {
     url = "https://github.com/mirage/fiat/releases/download/v${version}/${pname}-v${version}.tbz";
-    sha256 = "0086h9qkvnqfm8acrxqbki54z619nj73x7f0d01v5vg2naznx7w9";
+    sha256 = "1vf8xzs27yggspaz722wdn7gs80bs6vamkjiz2jbmh5h1j020apk";
   };
 
-  buildInputs = [ dune-configurator ];
+  # test is currently broken due to ppx_deriving.std's kind (ppx deriver)
+  # not being recognized correctly by dune 2.7
+  prePatch = ''
+    rm -rf test/helpers/wycheproof test/wycheproof/dune
+  '';
+
+  nativeBuildInputs = [ pkg-config dune-configurator ];
   propagatedBuildInputs = [ bigarray-compat cstruct eqaf hex ];
   checkInputs = [ alcotest asn1-combinators benchmark
                   ppx_deriving_yojson rresult stdlib-shims yojson ];
