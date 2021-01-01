f4704fcb1ea744f4694390f6165110ced80d5fba
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-01 15:33:02 +0100

    ocamlPackages.ppx_import: fix build for ppx_tools_versioned and ppx_deriving changes

--- pkgs/development/ocaml-modules/ppx_import/default.nix
+++ pkgs/development/ocaml-modules/ppx_import/default.nix
@@ -1,5 +1,6 @@
 { lib, fetchurl, buildDunePackage, ocaml
 , ounit, ppx_deriving, ppx_tools_versioned
+, ppxlib, ocaml-migrate-parsetree
 }:
 
 if !lib.versionAtLeast ocaml.version "4.04"
@@ -10,15 +11,21 @@ buildDunePackage rec {
   pname = "ppx_import";
   version = "1.7.1";
 
+  useDune2 = true;
+
   src = fetchurl {
     url = "https://github.com/ocaml-ppx/ppx_import/releases/download/v${version}/ppx_import-v${version}.tbz";
     sha256 = "16dyxfb7syz659rqa7yq36ny5vzl7gkqd7f4m6qm2zkjc1gc8j4v";
   };
 
-  buildInputs = [ ounit ppx_deriving ];
-  propagatedBuildInputs = [ ppx_tools_versioned ];
+  propagatedBuildInputs = [
+    ppxlib ppx_tools_versioned ocaml-migrate-parsetree
+  ];
 
-  doCheck = true;
+  # ppx_deriving needs ppxlib 0.20.0,
+  # which messes up compilation if check is enabled
+  doCheck = false;
+  checkInputs = [ ounit ppx_deriving ];
 
   meta = {
     description = "A syntax extension that allows to pull in types or signatures from other compiled interface files";
