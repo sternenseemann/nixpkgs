28b6ef6aa7b2605c386a176b88e81aa2091efc1c
Arthur Teisseire <arthurteisseire33@gmail.com>
2021-01-28 12:21:47 +0100

    ocaml-sha: init at 1.13

---
+++ pkgs/development/ocaml-modules/sha/default.nix
@@ -0,0 +1,20 @@
+{ lib
+, fetchgit
+, buildDunePackage
+, dune-configurator
+}:
+
+buildDunePackage rec {
+  pname = "sha";
+  version = "1.13";
+
+  useDune2 = true;
+
+  src = fetchgit {
+    url = "https://github.com/arthurteisseire/ocaml-${pname}.git";
+    sha256 = "1nfirappw3kspf45vp6f78lg36ksk30d7b2c7ymzqw71hybazs26";
+    rev = "355de5cba805b2d79e51a2636c2056e72f3f1136";
+  };
+
+  buildInputs = [ dune-configurator ];
+}
