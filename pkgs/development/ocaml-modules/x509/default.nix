814f9548a54b9093718ab7edca4c419389d9e124
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-04-05 13:38:46 +0200

    ocamlPackages.x509: 0.11.2 -> 0.12.0
    https://github.com/mirleft/ocaml-x509/releases/tag/v0.12.0

--- pkgs/development/ocaml-modules/x509/default.nix
+++ pkgs/development/ocaml-modules/x509/default.nix
@@ -1,32 +1,27 @@
 { lib, fetchurl, buildDunePackage, fetchpatch
 , alcotest, cstruct-unix
 , asn1-combinators, domain-name, fmt, gmap, rresult, mirage-crypto, mirage-crypto-pk
-, logs, base64
+, mirage-crypto-ec, mirage-crypto-rng, logs, base64, pbkdf
 }:
 
 buildDunePackage rec {
-  minimumOCamlVersion = "4.07";
+  minimumOCamlVersion = "4.08";
 
   pname = "x509";
-  version = "0.11.2";
+  version = "0.12.0";
 
   src = fetchurl {
     url = "https://github.com/mirleft/ocaml-x509/releases/download/v${version}/x509-v${version}.tbz";
-    sha256 = "1b4lcphmlyjhdgqi0brakgjp3diwmrj1y9hx87svi5xklw3zik22";
+    sha256 = "300529159fad2c1bca33920a3b5f691f3393b7670892a4c0075521ab914ce511";
   };
-
-  patches = [
-    # fix tests for mirage-crypto >= 0.8.9, can be removed at next release
-    (fetchpatch {
-      url = "https://github.com/mirleft/ocaml-x509/commit/ba1fdd4432950293e663416a0c454c8c04a71c0f.patch";
-      sha256 = "1rbjf7408772ns3ypk2hyw9v17iy1kcx84plr1rqc56iwk9zzxmr";
-    })
-  ];
 
   useDune2 = true;
 
   buildInputs = [ alcotest cstruct-unix ];
-  propagatedBuildInputs = [ asn1-combinators domain-name fmt gmap mirage-crypto mirage-crypto-pk rresult  logs base64 ];
+  propagatedBuildInputs = [
+    asn1-combinators domain-name fmt gmap mirage-crypto mirage-crypto-pk
+    mirage-crypto-ec mirage-crypto-rng rresult logs base64 pbkdf
+  ];
 
   doCheck = true;
 
